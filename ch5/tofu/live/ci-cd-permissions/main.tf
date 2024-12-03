3c630ba32ca3c1cb8d25d6c8a40e869b9e44dc64
Nesh Basnet <hsenesh@gmail.com>
2024-12-03 14:45:39 -0500

    remove the bucket for state

--- ch5/tofu/live/ci-cd-permissions/main.tf
+++ ch5/tofu/live/ci-cd-permissions/main.tf
@@ -18,13 +18,13 @@ module "iam_roles" {
   enable_iam_role_for_testing = true                            
 
   # TODO: fill in your own repo name here!
-  github_repo      = "/alchemist007/devops-book" 
+  github_repo      = "alchemist007/devops-book" 
   lambda_base_name = "lambda-sample"                            
 
-  enable_iam_role_for_plan  = true                                
-  enable_iam_role_for_apply = true                                
+  # enable_iam_role_for_plan  = true                                
+  # enable_iam_role_for_apply = true                                
 
   # TODO: fill in your own bucket and table name here!
-  tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
-  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
+  # tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
+  # tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
 }
